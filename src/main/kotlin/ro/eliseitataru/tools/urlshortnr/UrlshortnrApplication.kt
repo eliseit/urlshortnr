3e66fc8fe1a8c8827083c032501d7ac31bfc940b
Elisei TATARU <elisei.tataru@gmail.com>
2024-02-07 12:55:09 +0200

    [Elisei][SHR-1] Intial project setup, Kotlin and Maven

---
+++ src/main/kotlin/ro/eliseitataru/tools/urlshortnr/UrlshortnrApplication.kt
@@ -0,0 +1,11 @@
+package ro.eliseitataru.tools.urlshortnr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrlshortnrApplication
+
+fun main(args: Array<String>) {
+	runApplication<UrlshortnrApplication>(*args)
+}
